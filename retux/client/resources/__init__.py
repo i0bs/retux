196fd2be3b497307dd6301d4ad5b150f3f3ad400
James Walston <41456914+jameswalston@users.noreply.github.com>
2022-07-19 16:10:38 -0400

    chore: add `User` object to namespace import

--- retux/client/resources/__init__.py
+++ retux/client/resources/__init__.py
@@ -1,6 +1,7 @@
 from .abc import *  # noqa
 from .app_commands import *  # noqa
 from .components import *  # noqa
+from .user import *  # noqa
 
 # Resource modules to be added when all dependencies are added as well:
 # from .application
