69b5afeb68f625b354c9441439be20bfd77c19df
Dworv <88643996+Dworv@users.noreply.github.com>
2022-07-19 07:51:23 -0700

    docs: make ints `code` and fix typos
    Co-authored-by: i0 <41456914+i0bs@users.noreply.github.com>

--- retux/client/resources/channel.py
+++ retux/client/resources/channel.py
@@ -131,7 +131,7 @@ class Overwrite:
     """
     The type of overwrite.
 
-    User 0 for to overwrite a role permission and 1 to overwrite a member permission.
+    Use `0` for to overwrite a role's permission and `1` to overwrite a guild member's permission.
     """
     allow: str = field()
     """The bit set representing the overwrite's allowed permissions."""
