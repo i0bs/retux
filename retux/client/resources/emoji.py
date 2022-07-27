695f0e9fe0edc1967711e4819810deb968641d73
Dworv <dwarvyt@gmail.com>
2022-07-27 11:47:10 -0700

    feat: implement emoji class

---
+++ retux/client/resources/emoji.py
@@ -0,0 +1,70 @@
+from attrs import define, field
+from .abc import Object, Snowflake
+from .user import User
+from ...utils import optional_c
+
+@define(kwargs_only=False)
+class Emoji(Object):
+    """
+    Represents an Emoji from Discord.
+    
+    Attributes
+    ----------
+    id : `Snowflake`, optional
+        The ID of the Emoji.
+
+        Only availible on guild created emojis.
+    name : `str`, optional
+        The name of the emoji.
+
+        This is a unicode emoji if the emoji 
+        is standard, otherwise it is the name 
+        of the emoji.
+    roles : `list[Role]`, optional
+        A list of roles allowed to use this emoji.
+    user : `User`, optional
+        The user that created the emoji.
+    require_colons : `bool`, optional
+        Whether or not the emoji needs to be wrapped in colons.
+    managed : `bool`, optional
+        Whether or not the emoji is managed.
+    animated : `bool`, optional
+        Whether or not the emoji is animated.
+    availible : `bool`, optional
+        Whether or not the emoji is availible.
+
+        This may be false if the server that owns 
+        the emoji lost a level of boosting.
+    """
+    id: str | Snowflake | None = field(converter=optional_c(Snowflake), default=None)
+    """
+    The ID of the Emoji.
+    
+    Only availible on guild created emojis.
+    """
+    name: str | None = field(default=None)
+    """
+    The name of the emoji.
+    
+    This is a unicode emoji if the emoji 
+    is standard, otherwise it is the name 
+    of the emoji.
+    """
+    # roles: list[dict] | list[Role] | None = field(converter=optional_c(list_c(Role)))
+    # TODO: Implement Role object
+    # """A list of roles allowed to use this emoji."""
+    user: dict | User | None = field(converter=optional_c(User))
+    """The user that created the emoji."""
+    require_colons: bool | None = field(default=None)
+    """Whether or not the emoji needs to be wrapped in colons."""
+    managed: bool | None = field(default=None)
+    """Whether or not the emoji is managed."""
+    animated: bool | None = field(default=None)
+    """Whether or not the emoji is animated."""
+    availible: bool | None = field(default=None)
+    """
+    Whether or not the emoji is availible.
+    
+    This may be false if the server that owns 
+    the emoji lost a level of boosting.
+    """
